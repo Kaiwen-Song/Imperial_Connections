32187f60b7dcd47a4cf5b5b2e0c3525f2f4481a7
Kaiwen Song <kaiwen0602@gmail.com>
2015-05-19 14:19:17 +0100

    Initial Commit

---
+++ Imperial Connections/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Imperial Connections
+//
+//  Created by Kaiwen Song on 19/05/2015.
+//  Copyright (c) 2015 HJK Webapps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
