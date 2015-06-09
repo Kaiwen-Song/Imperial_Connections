d8ac3831d2e80271a021bcfe365832c07c64980a
hal213 <hal213@imperial.ac.uk>
2015-06-09 15:45:36 +0100

    Login screen done

--- Imperial Connections/LoginScreen.swift
+++ Imperial Connections/LoginScreen.swift
@@ -16,29 +16,24 @@ class LoginScreen: UIViewController {
     @IBOutlet weak var Password: UITextField!
 
     var successLogIn : Bool = true
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadFromCoreData()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func saveToCoreData() {
         let appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let context:NSManagedObjectContext = appDel.managedObjectContext!
         
-        let ent = NSEntityDescription.entityForName("user_id", inManagedObjectContext: context)
+        let ent = NSEntityDescription.entityForName("UserModel", inManagedObjectContext: context)
         var newUser = UserModel(entity: ent!, insertIntoManagedObjectContext: context)
         newUser.username = LoginField.text
         newUser.password = Password.text
         
         context.save(nil)
+        println("SAVED!!!")
         // to handle the NSErrorPointer, please modify
     }
     
@@ -46,30 +41,51 @@ class LoginScreen: UIViewController {
         let appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let context:NSManagedObjectContext = appDel.managedObjectContext!
         
-        let request = NSFetchRequest(entityName: "user_id")
+        let request = NSFetchRequest(entityName: "UserModel")
         request.returnsObjectsAsFaults = false;
-        request.predicate = NSPredicate(format: "username = %@", LoginField.text)
+        //request.predicate = NSPredicate(format: "username = %@", LoginField.text)
         var results:NSArray = context.executeFetchRequest(request, error: nil)!
-        if results.count > 0 {
-            //load
+        var isLoggedOn = results.count
+        if isLoggedOn > 0 { /* When the user is already logged on */
+            /* load the data from database and compare it with the core data using get_user */
             for user in results {
                 var thisUser = user as! UserModel
+                var id = thisUser.username
+                var password = thisUser.password
+                if (BackendServices().get_user(id, password: password)) {
+                    /* log in detail of core data matches the database */
+                    /* skip the log in page */
+                    self.performSegueWithIdentifier("ToTabScreen", sender: self)
+                    break
+                    
+                }
+                else {
+                    var alert = UIAlertController(title: "Log On Error", message: "Please check your log in details or your internet connection", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
             }
-        } else {
-            //log in page -> log in clicked (save)
         }
     }
+    
+    
+    
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 
     @IBAction func logOnButton(sender: AnyObject) {
         // Check login details with database 
         // If successful then goto tab screen, if not pop up error message
         let backend = BackendServices()
-        // successLogIn = backend.get_user(LoginField.text!, password: Password.text) HACKED
-        successLogIn = backend.get_user("Jeffrey", password: "11111")
+        successLogIn = backend.get_user(LoginField.text!, password: Password.text)
+       // successLogIn = backend.get_user("Jeffrey", password: "11111")
         if successLogIn {
             self.performSegueWithIdentifier("ToTabScreen", sender: self)
+            saveToCoreData()
         } else {
             var alert = UIAlertController(title: "Log On Error", message: "Please check your log in details or your internet connection", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
